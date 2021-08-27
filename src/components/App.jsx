a57a488939a155c114f7b74d2c2a325d71862f3e
whitewolf8383 <kandt.vandf@gmail.com>
2021-08-26 21:11:13 -0600

    Added mapping and contacts object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 
+// Data
+import contacts from '../contacts';
+
 // Components
 import Card from './Card';
 
-function App() {
+function createCard(contacts) {
   return(
     <Card 
-      name="Keith"
-      address="123 Main St"
-      email="something@email.com"
-      phone="970-456-1237"
+      key={contacts.id}
+      name={contacts.name}
+      address={contacts.address}
+      email={contacts.email}
+      phone={contacts.phone}
     />
+  );
+};
+
+function App() {
+  return(
+    // With Map Loop
+    <div>
+      {contacts.map(createCard)}
+    </div>
+    
+    // Without Map Loop
+    //<Card 
+    //  name="Keith"
+    //  address="123 Main St"
+    //  email="something@email.com"
+    //  phone="970-456-1237"
+    ///>
   );
 };
 
---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    id: 0,
+    name: "Keith",
+    address: "123 Main St",
+    email: "something@email.com",
+    phone: "970-779-2967"
+  },
+  {
+    id: 0,
+    name: "Kailin",
+    address: "123 Main St",
+    email: "something@email.com",
+    phone: "970-779-2967"
+  },
+  {
+    id: 0,
+    name: "Joesph",
+    address: "123 Main St",
+    email: "something@email.com",
+    phone: "970-779-2967"
+  }
+]
+
+module.exports = contacts;
